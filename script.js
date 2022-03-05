e054de7ac12ba67a0a6c06626488dfa1b89d543f
hablemoscrypto <hablemoscrypto.es@gmail.com>
2022-03-05 02:26:37 -0500

    Added Spanish

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cryptocurrency.", "Investing.", "Trading.", "NFTs."],
+        strings: ["Criptomonedas.", "Iversiones.", "Trading.", "NFTs."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Computer Scientist.", "Investor.", "Trader.", "NFT Lover."],
+        strings: ["Ingeniero de sistemas.", "Inversionista.", "Trader.", "NFT Lover."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
